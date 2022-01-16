33d4166f6e07dfb7624afbc188d48fc0d6a993c3
TheBeast <88646493+xootkama@users.noreply.github.com>
2022-01-16 07:28:11 +0530

    Update and rename aosp_kebab.mk to evolution_kebab.mk

--- evolution_kebab.mk
+++ evolution_kebab.mk
@@ -22,13 +22,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/kebab/device.mk)
 
 # Inherit some common Nameless-Aosp stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 # Boot Animation
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_kebab
+PRODUCT_NAME := evolution_kebab
 PRODUCT_DEVICE := kebab
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
